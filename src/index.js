50a667afa64218f2605e2238064cffcaf364d492
fribble186 <psj124@qq.com>
2021-08-24 17:46:42 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Demo from "./components/demo"
+
+ReactDOM.render(<div><Demo/></div>, document.getElementById("root"));
---
+++ webpack.config.dev.js
@@ -0,0 +1,52 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/, // jsx/js文件的正则
+        exclude: /node_modules/, // 排除 node_modules 文件夹
+        use: {
+          // loader 是 babel
+          loader: "babel-loader",
+          options: {
+            // babel 转义的配置选项
+            babelrc: false,
+            presets: [
+              // 添加 preset-react
+              require.resolve("@babel/preset-react"),
+              [require.resolve("@babel/preset-env"), { modules: false }],
+            ],
+            cacheDirectory: true,
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new htmlWebpackPlugin({
+      template: "public/index.html",
+    }),
+  ],
+};
